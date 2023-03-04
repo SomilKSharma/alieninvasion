28fe6b19215b6fca445c8d7a89f3f9f874870dcb
Somil Sharma <somil81@icloud.com>
2023-03-04 12:05:45 +0530

    initial commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """Store all settings"""
+    def __init__(self):
+        """Initialize settings"""
+        #Screen
+        self.screen_width=1200
+        self.screen_height=800
+        
+        self.ship_speed=1.5
+        self.bg_color=(230,230,230)
